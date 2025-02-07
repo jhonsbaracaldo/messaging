9794a60780f76d6127f629632ce52588b0f42b73
DnllaB <danielapb78@gmail.com>
2025-02-07 17:22:34 -0500

    se agrega la carpeta Dto User se insertan los atributos de la clase User y del Dto

--- src/main/java/efinomina/message/efinomina/domain/entitys/User.java
+++ src/main/java/efinomina/message/efinomina/domain/entitys/User.java
@@ -1,4 +1,30 @@
 package efinomina.message.efinomina.domain.entitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
 public class User {
+    public User(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_user;
+    private int id_area;
+    private int dni_user;
+    private String name_user;
+    private String lstname_user;
+    private String eml_user;
+
+
 }
